094575e819887d0fe4a0d8031407cfa37e14270f
Abhishek5723 <104137737+Abhishek5723@users.noreply.github.com>
2023-10-29 16:05:21 +0530

    Add files via upload

---
+++ Testimonials.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import Card  from './Card' 
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+
+export default function Testimonials(props) {
+    let reviews = props.reviews;
+    const[index,setIndex]=useState(0)
+    function leftShiftHandler(){
+    if(index-1 <0){
+    setIndex(reviews.length-1);
+    }
+    else{
+        setIndex(index-1);
+    }
+    }
+
+    function rightShiftHandler(){
+        if(index+1 >= reviews.length){
+            setIndex(0);
+                }
+                else{
+                    setIndex(index+1);
+                }
+
+    }
+
+    function surpriseHandler(){
+        let randomIndex =Math.floor(Math.random() * reviews.length);
+        // Now you can use randomValue for something meaningful.
+        setIndex(randomIndex);
+    }
+
+  return (
+    <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10 transition-all duration-700 hover:shadow-2xl rounded-md'>
+        <Card review ={reviews[index]}></Card>
+
+        <div className='flex text-3xl mt-9 gap-3 text-violet-400 font-bold justify-center'>
+    <button onClick={leftShiftHandler}
+    className='cursor-pointer hover:text-violet-500 '>
+        <FiChevronLeft/>
+    </button>
+    <button  onClick={rightShiftHandler}
+     className='cursor-pointer hover:text-violet-500 '>
+        <FiChevronRight/>
+    </button>
+</div>
+
+<div>
+    <button onClick={surpriseHandler}
+    className='bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-12 py-3 mt-6 rounded-md font-bold text-lg text-white'>
+        Surprise ME
+    </button>
+</div>
+
+
+    </div>
+    
+  )
+}
